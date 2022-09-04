54cd3c5c02d950026fa970a41576ac6796487f45
Chandra kishor Gauro <ckgauro@gmail.com>
2022-09-03 22:44:02 -0400

    08-SpringBoot-Internationalization

--- springboot-buildingblocks/src/main/java/com/gauro/restservices/springbootbuildingblocks/restservices/hello/HelloWorldController.java
+++ springboot-buildingblocks/src/main/java/com/gauro/restservices/springbootbuildingblocks/restservices/hello/HelloWorldController.java
@@ -1,13 +1,23 @@
 package com.gauro.restservices.springbootbuildingblocks.restservices.hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 /**
  * @author Chandra
  */
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private ResourceBundleMessageSource messageSource;
 
     @GetMapping("/helloworld")
     public String helloWorld(){
@@ -27,6 +37,16 @@ public class HelloWorldController {
                 .city("KTM")
                 .lastName("Reddy")
                 .build();
+    }
+
+    @GetMapping("/hello-int")
+    public  String getMessageInt18NFormat(@RequestHeader(name="Accept-language", required = false) String local){
+        return messageSource.getMessage("label.hello",null, new Locale(local));
+    }
+    @GetMapping("/hello-int2")
+    public String getMessagesInI18NFormat2() {
+        return messageSource.getMessage("label.hello", null, LocaleContextHolder.getLocale());
+
     }
 
 }
